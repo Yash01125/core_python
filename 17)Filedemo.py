ce4152ac4f8146a6e1f138a3ee1108baf00763cd
Yash01125 <mordharay@gmail.com>
2024-07-25 09:22:32 +0530

    25/07/2024

--- 17)Filedemo.py
+++ 17)Filedemo.py
@@ -7,15 +7,23 @@ data.close()
 data=open('data.txt','r')
 even=open('even.txt','w')
 odd=open('odd.txt','w')
+prime=open('prime.txt','w')
 l=data.read().split(',')[:-1]
 for i in l:
     if int(i)%2==0:
         even.write(i+',')
     else:
         odd.write(i+',')
+    if int(i)%2!=0:
+        for j in range(3,int(int(i)/2)+1,2):
+            if int(i)%j==0:
+                break
+            else:
+                prime.write(i+',')
 data.close()
 even.close()
 odd.close()
+prime.close()
 
 data=open('data.txt','r')
 print(data.read())
@@ -26,5 +34,6 @@ even.close()
 odd=open('odd.txt','r')
 print(odd.read())
 odd.close()
-
-
+prime=open('prime.txt','r')
+print(prime.read())
+prime.close()
--- prime number.py
+++ prime number.py
@@ -1,7 +1,7 @@
 n=int(input("Enter N:"))
 
 if n%2!=0:
-n    for i in range(3,int(n/2)+1,2):
+    for i in range(3,int(n/2)+1,2):
         if n%i==0:
             print(n,"is not prime")
             break
