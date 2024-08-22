9068c83389c1ac377572cde50478ff9ed85d4550
Yash01125 <mordharay@gmail.com>
2024-08-22 08:41:59 +0530

    08/22/2024

---
+++ 29)Numpy_array.py
@@ -0,0 +1,22 @@
+import numpy as np
+'''array=np.zeros([5])
+print(array)'''
+
+# 2) Create a NumPy array with 3 rows and 4 columns initialized with ones.
+'''array=np.ones([3,4])
+print(array)'''
+
+# 3) Create a 1D NumPy array containing numbers from 1 to 10.
+'''a=np.arange(1,11)
+print(a)'''
+
+# 4) Create a 2D NumPy array with dimensions 3x3 containing numbers from 1 to 9.
+# a=np.random.randint(1,10,size=(3,3))
+# print(a)
+
+# 5) Create a 3D NumPy array with dimensions 2x3x4 containing random integers between 0 and 9.
+a=np.random.randint(0,10,size=(3,3,4))
+print(a)
+
+
+
